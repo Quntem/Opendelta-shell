3bea715a7a51e1d8533042e68d3583a7131159c0
Oscar May <67429870+OscarMayReal@users.noreply.github.com>
2024-04-10 21:41:23 +0100

    added window list testing

--- scripts/WinFunc.js
+++ scripts/WinFunc.js
@@ -2,3 +2,10 @@ function closewin(elementid) {
     const element = document.getElementById(elementid);
     element.remove();
 }
+function openwins() {
+    var windowDivs = document.querySelectorAll('div.window');
+    var windowIds = Array.from(windowDivs).map(function(div) {
+        return div.id;
+    });
+    return windowIds
+}
