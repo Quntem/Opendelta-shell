19ef5c7ab94e32e4273953466fa3693a042d801c
Oscar May <67429870+OscarMayReal@users.noreply.github.com>
2024-04-11 12:20:19 +0100

    redesigned dragging, added window creation

--- scripts/WinFunc.js
+++ scripts/WinFunc.js
@@ -9,3 +9,22 @@ function openwins() {
     });
     return windowIds
 }
+function newwin() {
+    fetch('elements/window.html')
+        .then(response => response.text())
+        .then(html => {
+            const newid = new Date().getTime().toString();
+            const desktop = document.getElementById('desktop');
+            desktop.innerHTML += html;
+            const newWindow = desktop.querySelector('#newwindow');
+            newWindow.id = "window" + newid;
+            const newTitlebar = desktop.querySelector('#newwindowtitlebar');
+            newTitlebar.id = "titlebar" + newid;
+            $( "#window" + newid ).draggable({ handle: "#titlebar" + newid });
+            $('.window').draggable({ handle: ".titlebar" });
+            const closeIcon = newWindow.querySelector('#closebutton');
+            closeIcon.setAttribute('onclick', `closewin('window${newid}')`);
+            closeIcon.id = "closeicon" + newid;
+        })
+        .catch(error => console.error('Error fetching HTML:', error));
+}
