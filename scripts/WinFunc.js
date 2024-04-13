1058c9bd01897a4f4b8db2b7ee6236e06a49feb9
Oscar May <67429870+OscarMayReal@users.noreply.github.com>
2024-04-13 11:19:05 +0100

    terminal addition

--- scripts/WinFunc.js
+++ scripts/WinFunc.js
@@ -28,3 +28,20 @@ function newwin() {
         })
         .catch(error => console.error('Error fetching HTML:', error));
 }
+function termlog(text) {
+    const terminal = document.getElementById('termoutput');
+    terminal.innerHTML += '<div class="mt-2 bg-gray-500/10 backdrop-blur-lg text-sm text-white rounded-lg p-4 mb-5 ml-5 mr-5" role="alert"><span class="font-bold">Javascript:</span>' + ' ' + text + '</div>'
+}
+function termruncode(event) {
+    code = document.getElementById("ConsoleTB").value;
+    if (event.keyCode === 13) {
+        const terminal = document.getElementById('termoutput');
+        terminal.innerHTML += '<div class="mt-2 bg-gray-500/10 backdrop-blur-lg text-sm text-white rounded-lg p-4 mb-5 ml-5 mr-5" role="alert"><span class="font-bold">User:</span>' + ' ' + code + '</div>'
+        try {
+            eval(code);
+        } catch (error) {
+            termlog(error);
+        };
+        document.getElementById("ConsoleTB").value = "";
+    };
+}
