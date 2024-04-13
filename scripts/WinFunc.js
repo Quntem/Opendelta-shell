afc55ed7354ced6e23aec15e610e647acb5873f8
Oscar May <67429870+OscarMayReal@users.noreply.github.com>
2024-04-13 11:36:20 +0100

    added newwinfromurl() to create a new window from a hosted html file

--- scripts/WinFunc.js
+++ scripts/WinFunc.js
@@ -28,15 +28,34 @@ function newwin() {
         })
         .catch(error => console.error('Error fetching HTML:', error));
 }
+function newwinfromurl(winurl) {
+    fetch(winurl)
+        .then(response => response.text())
+        .then(html => {
+            const newid = new Date().getTime().toString();
+            const desktop = document.getElementById('desktop');
+            desktop.innerHTML += html;
+            const newWindow = desktop.querySelector('#newwindow');
+            newWindow.id = "window" + newid;
+            const newTitlebar = desktop.querySelector('#newwindowtitlebar');
+            newTitlebar.id = "titlebar" + newid;
+            $( "#window" + newid ).draggable({ handle: "#titlebar" + newid });
+            $('.window').draggable({ handle: ".titlebar" });
+            const closeIcon = newWindow.querySelector('#closebutton');
+            closeIcon.setAttribute('onclick', `closewin('window${newid}')`);
+            closeIcon.id = "closeicon" + newid;
+        })
+        .catch(error => console.error('Error fetching HTML:', error));
+}
 function termlog(text) {
     const terminal = document.getElementById('termoutput');
-    terminal.innerHTML += '<div class="mt-2 bg-gray-500/10 backdrop-blur-lg text-sm text-white rounded-lg p-4 mb-5 ml-5 mr-5" role="alert"><span class="font-bold">Javascript:</span>' + ' ' + text + '</div>'
+    terminal.innerHTML += '<div class="mt-2 bg-gray-500/10 backdrop-blur-lg text-sm text-white rounded-lg p-4 mb-5 ml-5 mr-5 break-words" role="alert"><span class="font-bold">Javascript:</span>' + ' ' + text + '</div>'
 }
 function termruncode(event) {
     code = document.getElementById("ConsoleTB").value;
     if (event.keyCode === 13) {
         const terminal = document.getElementById('termoutput');
-        terminal.innerHTML += '<div class="mt-2 bg-gray-500/10 backdrop-blur-lg text-sm text-white rounded-lg p-4 mb-5 ml-5 mr-5" role="alert"><span class="font-bold">User:</span>' + ' ' + code + '</div>'
+        terminal.innerHTML += '<div class="mt-2 bg-gray-500/10 backdrop-blur-lg text-sm text-white rounded-lg p-4 mb-5 ml-5 mr-5 break-words" role="alert"><span class="font-bold">User:</span>' + ' ' + code + '</div>'
         try {
             eval(code);
         } catch (error) {
