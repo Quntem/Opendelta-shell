6970ba68f9fc0890f14ff35eaf14287109bbb0f2
Oscar May <67429870+OscarMayReal@users.noreply.github.com>
2024-04-14 14:14:39 +0100

    some features to communicate with Delta-BE

--- scripts/WinFunc.js
+++ scripts/WinFunc.js
@@ -64,3 +64,38 @@ function termruncode(event) {
         document.getElementById("ConsoleTB").value = "";
     };
 }
+function termgetpermission(permission, reason) {
+    const terminal = document.getElementById('termoutput');
+    terminal.innerHTML += '<div class="ml-5 mr-5 mb-5 bg-blue-100 border border-blue-200 text-gray-800 rounded-lg p-4 dark:bg-blue-800/10 dark:border-blue-900 dark:text-white" role="alert"> <div class="flex"> <div class="flex-shrink-0"> <svg class="flex-shrink-0 size-4 mt-1" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"> <circle cx="12" cy="12" r="10"></circle> <path d="M12 16v-4"></path> <path d="M12 8h.01"></path> </svg> </div> <div class="ms-3"> <h3 class="font-semibold"> This Script would like to access' + ' ' + permission + ' </h3> <div class="mt-2 text-sm text-gray-600 dark:text-gray-400"> ' + reason + ' </div> <div class="mt-4"> <div class="flex space-x-3"> <button type="button" class="inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent text-blue-600 hover:text-blue-800 disabled:opacity-50 disabled:pointer-events-none dark:text-blue-500 dark:hover:text-blue-400"> Deny </button> <button type="button" class="inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent text-blue-600 hover:text-blue-800 disabled:opacity-50 disabled:pointer-events-none dark:text-blue-500 dark:hover:text-blue-400"> Allow </button> </div> </div> </div> </div> </div>'
+}
+function getfiles(fileurl) {
+    files = {}
+    fetch(fileurl)
+        .then(response => response.json())
+        .then (data => {
+            termlog(data)
+        })
+    return files
+}
+function ls() {
+    files = []
+    fetch(fileserver + "/api/files/list")
+        .then(response => response.json())
+        .then (data => {
+            for(let i = 0; i < data.length; i++) {
+                files += data[i] + ", ";
+            };
+            termlog(files);
+        })
+    return files
+}
+function sf(filename) {
+    files = []
+    fetch(fileserver + "/api/files/get/?file=" + filename)
+        .then(response => response.json())
+        .then (response => {
+            termlog(response.data);
+            console.log(response)
+        })
+    return files
+}
