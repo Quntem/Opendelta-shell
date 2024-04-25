2cd828f8de38e6ac35477aae0a35b2e109a158e9
Oscar May <67429870+OscarMayReal@users.noreply.github.com>
2024-04-25 08:30:52 +0100

    text edit app, file drag and drop, launch arguments, better backend file implementaion

--- scripts/Apps.js
+++ scripts/Apps.js
@@ -11,14 +11,14 @@ function listapps() {
         })
 }
 function drawtbapps(taskbar) {
-    taskbar = document.getElementById(taskbar)
+    newtaskbar = document.getElementById(taskbar)
     fetch('apps/apps.json')
         .then(response => response.json())
         .then(response => {
             appslist = response.apps
             appsnamelist = []
             appslist.forEach(element => {
-                taskbar.innerHTML += ' <img src="' + element.icon + '" width="30px" height="30px" class="ml-2 mr-2" onclick="newwinfromurl(' + "'" + element.mainwin + "'" + ')"></img>'
+                newtaskbar.innerHTML += ' <img draggable="false" src="' + element.icon + '" width="30px" height="30px" class="ml-2 mr-2" onclick="newwinfromurl(' + "'" + element.mainwin + "'" + ')"></img>'
             });
         })
 }
--- scripts/Files.js
+++ scripts/Files.js
@@ -3,13 +3,61 @@ function createfileitems(id) {
     fileview = document.getElementById(id)
     fileview.innerHTML = ""
     console.log("test")
-    fetch(fileserver + "/api/files/list/?dir=" + currentdir)
+    fetch(fileserver + "/api/files/listenhanced/?dir=" + currentdir)
         .then(response => response.json())
         .then (data => {
             for(let i = 0; i < data.length; i++) {
-                fileview.innerHTML += '<div draggable="true" tabindex="1" class="fileapp-fileitem grid justify-items-center pt-4 pl-4 pr-4 pb-4 mr-2 ml-2 mt-2 rounded-lg hover:bg-slate-50 focus:bg-slate-100" style="font-size: 10px; max-width: min-content; justify-items: center; display: inline-grid;"><img class="pl-2 pr-2 pt-2 pb-2" src="apps/files/assets/fileicon.png" width="50px" height="50px"></img><div style="font-size: 15px;" class="poppins-medium">' + data[i] + '</div></div>'
+                fn = data[i].name
+                let icon = ""
+                if(data[i].type == "directory") {
+                    icon = 'apps/files/assets/foldericon.png" width="70px" height="70px"'
+                    itemonclick = ''
+                }
+                if(data[i].type == "file") {
+                    icon = 'apps/files/assets/fileicon.png" width="50px" height="50px"'
+                    itemonclick = ''
+                }
+                fileview.innerHTML += '<div draggable="true" fn="' + fn + '" onclick="' + itemonclick + '" ondragstart="drag(event)" tabindex="1" class="fileapp-fileitem grid justify-items-center pt-4 pl-4 pr-4 pb-4 mr-2 ml-2 mt-2 rounded-lg hover:bg-slate-50 focus:bg-slate-100" style="font-size: 10px; max-width: min-content; justify-items: center; display: inline-grid;"><img class="pl-2 pr-2 pt-2 pb-2" draggable="false" src="' + icon + '></img><div style="font-size: 15px;" class="poppins-medium">' + fn + '</div></div>'
             };
         });
+}
+function drag(ev) {
+    filename = ev.target.getAttribute("fn")
+    ev.dataTransfer.setData("text", filename);
+}
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    console.log(ev.target.id)
+    console.log(data)
+    document.getElementById(ev.target.id).value = data
+}
+function texteditdrop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    console.log(ev.target.id)
+    console.log(data)
+    fetch(fileserver + "/api/files/get/?file=" + data)
+        .then(response => response.json())
+        .then (response => {
+            document.getElementById(ev.target.id).value = response.data
+            document.getElementById("teditwindowtitle").innerHTML = "Text Editor - " + data
+            document.getElementById("savebutton").setAttribute("filename", data)
+            document.getElementById("savebutton").setAttribute("textbox", ev.target.id)
+        })
+}
+function acceptdrop(ev) {
+    ev.preventDefault();
+}
+function savefile(ev){
+    file = document.getElementById(ev.target.id).getAttribute("filename")
+    textbox = document.getElementById(ev.target.id).getAttribute("textbox")
+    filedata = document.getElementById(textbox).value
+    fetch(fileserver + "/api/files/savefile?file=" + file + "&filedata=" + filedata)
+        .then(response => response.json())
+        .then (response => {
+
+        })
 }
 $(function() {
     $.contextMenu({
@@ -21,13 +69,9 @@ $(function() {
         items: {
             "edit": {name: "Edit", icon: "edit"},
             "cut": {name: "Cut", icon: "cut"},
-           copy: {name: "Copy", icon: "copy"},
+            "copy": {name: "Copy", icon: "copy"},
             "paste": {name: "Paste", icon: "paste"},
             "delete": {name: "Delete", icon: "delete"},
-            "sep1": "---------",
-            "quit": {name: "Quit", icon: function(){
-                return 'context-menu-icon context-menu-icon-quit';
-            }}
         }
     });
 
